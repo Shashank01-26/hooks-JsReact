ad9c6bed0d8d28ea49327e8cab668160456276d1
Shashank Kumar Srivastava <srivastavashashank033@gmail.com>
2023-07-24 18:04:11 +0530

    todo useReducer

--- src/App.js
+++ src/App.js
@@ -192,62 +192,68 @@
 
 // }
 
-
-import React, { useState, useReducer } from 'react';
-import Todo from './Todo';
-
-export const ACTIONS = {
-  ADD_TODO: 'add-todo',
-  TOGGLE_TODO: 'toggle-todo',
-  DELETE_TODO: 'delete-todo'
-}
-
-function reducer(todos, action) {
-  switch (action.type) {
-    case ACTIONS.ADD_TODO:
-      return [...todos, newTodo(action.payload.name)]
-    case ACTIONS.TOGGLE_TODO:
-      return todos.map(todo => {
-        if (todo.id === action.payload.id) {
-          return { ...todo, complete: !todo.complete }
-        }
-        return todo;
-    })
-    case ACTIONS.DELETE_TODO:
-    return todos.filter(todo => todo.id !== action.payload.id)
-    default:
-  return todos;
-}
-
-}
-
-function newTodo(name) {
-  return { id: Date.now(), name: name, complete: false }
-}
+// __________________________________
+// |    TODO_APP                     |                           
+// |            USING                |             
+// |                  useReducer     |
+// |_________________________________| 
 
 
-export default function App() {
-  const [state, dispatch] = useReducer(reducer, [])
-  const [name, setName] = useState('')
+// import React, { useState, useReducer } from 'react';
+// import Todo from './Todo';
 
-  function handleSubmit(e) {
-    e.preventDefault()
-    dispatch({ type: ACTIONS.ADD_TODO, payload: { name: name } })
-    setName('')
-  }
+// export const ACTIONS = {
+//   ADD_TODO: 'add-todo',
+//   TOGGLE_TODO: 'toggle-todo',
+//   DELETE_TODO: 'delete-todo'
+// }
 
-  console.log(state)
+// function reducer(todos, action) {
+//   switch (action.type) {
+//     case ACTIONS.ADD_TODO:
+//       return [...todos, newTodo(action.payload.name)]
+//     case ACTIONS.TOGGLE_TODO:
+//       return todos.map(todo => {
+//         if (todo.id === action.payload.id) {
+//           return { ...todo, complete: !todo.complete }
+//         }
+//         return todo;
+//     })
+//     case ACTIONS.DELETE_TODO:
+//     return todos.filter(todo => todo.id !== action.payload.id)
+//     default:
+//   return todos;
+// }
 
-  return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={name} onChange={e => setName(e.target.value)}></input>
-      </form>
-      {state.map(todo => {
-        return <Todo key={todo.id} todo={todo} dispatch={dispatch} />
+// }
 
-      })}
-    </>
-  )
+// function newTodo(name) {
+//   return { id: Date.now(), name: name, complete: false }
+// }
 
-}
+
+// export default function App() {
+//   const [state, dispatch] = useReducer(reducer, [])
+//   const [name, setName] = useState('')
+
+//   function handleSubmit(e) {
+//     e.preventDefault()
+//     dispatch({ type: ACTIONS.ADD_TODO, payload: { name: name } })
+//     setName('')
+//   }
+
+//   console.log(state)
+
+//   return (
+//     <>
+//       <form onSubmit={handleSubmit}>
+//         <input type="text" value={name} onChange={e => setName(e.target.value)}></input>
+//       </form>
+//       {state.map(todo => {
+//         return <Todo key={todo.id} todo={todo} dispatch={dispatch} />
+
+//       })}
+//     </>
+//   )
+
+// }
