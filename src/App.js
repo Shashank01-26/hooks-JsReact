e741238a00a2c0f31c8d972012d8b556af3515d4
Shashank Kumar Srivastava <srivastavashashank033@gmail.com>
2023-07-27 00:45:09 +0530

    useCallback implementation

--- src/App.js
+++ src/App.js
@@ -257,3 +257,34 @@
 //   )
 
 // }
+
+
+import React, { useState, useCallback } from 'react'
+import List from './List.js'
+
+export default function App(){
+    const [number,setNumber] = useState(1)
+    const [dark,setDark] = useState(false)
+
+    const getItems = useCallback((incremer)=>{
+        return [number+incremer, number+incremer+1, number+2+incremer]
+    },[number])
+
+
+const theme = {
+    backgroundColor:dark? '#333':'#fff',
+    color:dark?'#fff': '#333',
+}
+return(
+    <div style ={theme}>
+<input 
+type='number'
+value={number}
+onChange={e => setNumber(parseInt(e.target.value))}
+/>
+<button onClick={()=> setDark(prevDark=> !prevDark)}>Toggle theme</button>
+<List getItems={getItems}/>
+    </div>
+)
+
+}
---
+++ src/List.js
@@ -0,0 +1,12 @@
+import React,{useEffect,useState} from 'react'
+
+export default function List({getItems}){
+    const [items,setitems] = useState([])
+
+    useEffect(()=>{
+        setitems(getItems(5))
+        console.log('Updating.....')
+    },[getItems])
+
+    return items.map(item=> <div key={item}>{item}</div>)
+}
